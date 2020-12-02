cccc70a4aa43e3b160b5f5bb6dd2c6fe967f6089
shaunakg <gadkari.shaunak@gmail.com>
2020-12-02 18:21:15 +1100

    made it actually load an image

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const webport = process.env.PORT || 8080;
 app.get("/log.png", (req, res) => {
     console.log("---- RECIEVED REQUEST ----")
     console.dir(req);
+
+    res.redirect("https://noto-website-2.storage.googleapis.com/emoji/emoji_u1f603.png")
+
 })
 
 http.listen(webport, function(){
