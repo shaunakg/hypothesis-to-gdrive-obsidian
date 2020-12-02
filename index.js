f67638992dbd11dd388e5006edb29688af6977ac
shaunakg <gadkari.shaunak@gmail.com>
2020-12-02 19:02:29 +1100

    fixed JSON

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var app = express();
 var http = require('http').createServer(app);
 const webport = process.env.PORT || 8080;
 
+app.use(express.bodyParser());
+
 app.post("/hook/in", (req, res) => {
     console.log("---- Recieved hook! ----")
+
     console.dir(req.body);
     res.end();
+    
 })
 
 http.listen(webport, function(){
